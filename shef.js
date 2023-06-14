27783805a3277f22c62957052e6bb82f411ed620
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-14 15:09:41 +1000

    3.1,2. Add a data store and implementation

--- shef.js
+++ shef.js
@@ -10,7 +10,26 @@
  * Copyright (C) 2023 Miguel Guthridge
  */
 
-const data = null; // TODO: Create a data store
+let data = {
+  nextOrderNumber: 1, // The next order number to use
+  orders: [], // An array of the current orders
+};
+
+/**
+ * Helper function for looking up orders.
+ *
+ * In big projects, helper functions like this are extremely useful for writing
+ * readable and maintainable code.
+ */
+function findOrder(orderNumber) {
+  for (const order of data.orders) {
+    if (order.orderNumber === orderNumber) {
+      return order;
+    }
+  }
+  // If we reach the end, say that there was no result by returning null
+  return null;
+}
 
 /**
  * Place an order, returning the order number for the given order.
@@ -22,8 +41,19 @@ const data = null; // TODO: Create a data store
  * was successful, otherwise an error message.
  */
 export function placeOrder(items) {
+  if (items.length === 0) {
+    return { error: 'Order contains no items' };
+  }
+  // Increment the order number after storing the current value
+  const orderNumber = data.nextOrderNumber++;
+  // Add the new order to the data
+  data.orders.push({
+    orderNumber,
+    items,
+    complete: false,
+  });
   return {
-    orderNumber: 1,
+    orderNumber: orderNumber,
   };
 }
 
@@ -37,9 +67,13 @@ export function placeOrder(items) {
  * of items in the completed order, or an error message.
  */
 export function orderDetails(orderNumber) {
+  const order = findOrder(orderNumber);
+  if (order === null) {
+    return { error: 'Order number is invalid' };
+  }
   return {
-    items: ['Tofu curry laksa'],
-    complete: false,
+    items: order.items,
+    complete: order.complete,
   };
 }
 
@@ -50,12 +84,18 @@ export function orderDetails(orderNumber) {
  * @returns {{ orderNumber: number, items: string[] }[]} an array of orders.
  */
 export function listOrders() {
-  return [
-    {
-      orderNumber: 1,
-      items: ['Tofu curry laksa'],
-    },
-  ];
+  const currentOrders = [];
+  // Filter the orders
+  for (const order of data.orders) {
+    // Only include the ones that aren't complete
+    if (!order.complete) {
+      currentOrders.push({
+        orderNumber: order.orderNumber,
+        items: order.items,
+      });
+    }
+  }
+  return currentOrders;
 }
 
 /**
@@ -68,6 +108,16 @@ export function listOrders() {
  * @returns {object | { error: string }} empty object, or an error message.
  */
 export function completeOrder(orderNumber) {
+  const order = findOrder(orderNumber);
+  if (order === null) {
+    return { error: 'Order number is invalid' };
+  }
+  if (order.complete) {
+    return { error: 'Order has already been completed' };
+  }
+  // Since JS uses pass-by-reference for arrays and objects, modifying the
+  // value here will also affect the data store
+  order.complete = true;
   return {};
 }
 
@@ -75,5 +125,9 @@ export function completeOrder(orderNumber) {
  * Resets the program to its initial state.
  */
 export function clear() {
-  return;
+  // Reset the data
+  data = {
+    nextOrderNumber: 1,
+    orders: [],
+  };
 }
