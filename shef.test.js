5a8840f807fd47ecf8397eaa2c902022b3e2394b
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-14 14:40:01 +1000

    2.1. Write test list

--- shef.test.js
+++ shef.test.js
@@ -9,3 +9,30 @@
  *
  * Copyright (C) 2023 Miguel Guthridge
  */
+
+describe('placeOrder', () => {
+  test.todo('Places order when given valid items');
+  test.todo('Order numbers are unique');
+  test.todo('Error when order contains no items');
+});
+
+describe('orderDetails', () => {
+  test.todo('Gives info for orders');
+  test.todo('Gives an error for invalid order numbers');
+});
+
+describe('listOrders', () => {
+  test.todo('Lists placed orders');
+  test.todo('Gives empty array when no orders are placed');
+  test.todo("Doesn't include orders that are already completed");
+});
+
+describe('completeOrder', () => {
+  test.todo('Marks order as completed');
+  test.todo('Gives an error for invalid order numbers');
+  test.todo('Gives an error for already-completed orders');
+});
+
+describe('clear', () => {
+  test.todo('Orders are deleted');
+});
